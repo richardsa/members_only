7f0b27ccd1ac96c4b8da735e35de571249e9388e
richie <rsaldiva@gmail.com>
2015-09-09 13:26:40 -0400

    add remember me token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   has_secure_password
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
+
